a47564545081597a30d65c87b8723b2fc799979f
İsa Diliballı <isadiliballi@icloud.com>
2020-05-17 01:12:38 +0300

    Initial Commit

---
+++ iEmotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iEmotion
+//
+//  Created by İsa Diliballı on 17.05.2020.
+//  Copyright © 2020 İsa Diliballı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
